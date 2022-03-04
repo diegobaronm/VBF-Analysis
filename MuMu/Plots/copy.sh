75f9f9bcb1ee0d4770e5989b24d24c8c2c09a257
Diego Baron <diego.baron@cern.ch>
2022-03-04 17:00:46 +0000

    Little changes to the plotting scripts.

--- MuMu/Plots/copy.sh
+++ MuMu/Plots/copy.sh
@@ -3,9 +3,11 @@ cp ../DATA/out/data.root Data.root
 cp ../MC/out/VV.root .
 cp ../MC/out/singletop.root .
 cp ../MC/out/Wjets.root .
-cp ../MC/out/Signal.root .
+cp ../MC/out/Signal_old.root .
+cp ../MC/out/Signal_Sherpa.root .
+cp ../MC/out/Signal_PoPy.root .
 cp ../MC/out/ttbar.root ttbar.root
 cp ../MC/out/Zmumu_Sherpa.root .
 cp ../MC/out/Zmumu_PoPy.root .
 hadd BG.root VV.root singletop.root Wjets.root ttbar.root Zmumu_Sherpa.root
-hadd MC.root BG.root Signal.root
+hadd MC.root BG.root Signal_Sherpa.root
