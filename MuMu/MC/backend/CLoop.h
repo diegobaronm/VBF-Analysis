01a31f84451161a1d567ba75269f6ffcc4c5f744
Diego Baron <diego.baron@cern.ch>
2023-05-20 19:23:05 +0200

    Now Zll running with new histogram containers.

--- MuMu/MC/backend/CLoop.h
+++ MuMu/MC/backend/CLoop.h
@@ -23,6 +23,7 @@
 
 // Header file for the classes stored in the TTree if any.
 #include "TLorentzVector.h"
+#include "../../../AnalysisCommons/histogramContainer.h"
 
 class CLoop {
   void Style(double lumFactor);
