d1ad9d44882c86dbdd1343aa37e5b5f70a83c014
Diego Baron <diego.baron@cern.ch>
2023-06-24 23:10:11 +0200

    Zll final states have new RW procedure.

--- Zee/MC/backend/CLoop_SR.C
+++ Zee/MC/backend/CLoop_SR.C
@@ -2,43 +2,7 @@
 
 #include "../Analysis.C"
 #include <cmath>
-#include <map>
-
-double mjj_rw_quadratic(double mjj, double a, double b, double c){
-    double rw = a*mjj*mjj+b*mjj+c;
-    if (rw<0){
-        return 0.0;
-    } else {
-        return rw;
-    }
-}
-
-double mjj_rw_linear(double mjj, double slope, double level){
-    double rw = slope*mjj+level;
-    if (rw<0){
-        return 0.0;
-    } else {
-        return rw;
-    } 
-}
-
-enum class MC
-{
-    PowHegPythia = 1,
-    SHERPA,
-    MadGraph,
-    SHERPANLO,
-    MadGraphNLO,
-};
-
-enum class Region
-{
-    DefaultNoRW,
-    SR,
-    CRa,
-    CRb,
-    CRc
-};
+#include "../../../AnalysisCommons/rewightingTools.h"
 
 void CLoop::Loop(double lumFactor, int z_sample, std::string key)
 {
@@ -206,72 +170,24 @@ void CLoop::Loop(double lumFactor, int z_sample, std::string key)
         else if ((z_centrality>=0.5 && z_centrality<=1) && n_jets_interval==1){region = Region::CRb;}
         else if ((z_centrality>=0.5 && z_centrality<=1) && n_jets_interval==0){region = Region::CRc;}
 
-        std::map<Region,std::vector<double>> parametersSHERPA = {
-            {Region::DefaultNoRW,{0.0,0.0,1.0}},
-            {Region::SR,{1.40E-07,-7.16E-04,1.51E+00}},
-            {Region::CRa,{5.61E-08,-4.20E-04,1.25E+00}},
-            {Region::CRb,{4.12E-08,-3.64E-04,1.08E+00}},
-            {Region::CRc,{1.09E-07,-6.10E-04,1.30E+00}}
-        };
-
-        std::map<Region,std::vector<double>> parametersMadGraph = {
-            {Region::DefaultNoRW,{0.0,0.0,1.0}},
-            {Region::SR,{1.30E-07,-5.29E-04,9.82E-01}},
-            {Region::CRa,{1.53E-07,-5.42E-04,1.10E+00}},
-            {Region::CRb,{6.24E-08,-2.97E-04,9.72E-01}},
-            {Region::CRc,{5.95E-08,-3.32E-04,8.78E-01}}
-        };
-
-        std::map<Region,std::vector<double>> parametersSHERPANLO = {
-            {Region::DefaultNoRW,{0.0,0.0,1.0}},
-            {Region::SR,{9.15E-08,-4.62E-04,1.21E+00}},
-            {Region::CRa,{5.81E-08,-2.63E-04,1.21E+00}},
-            {Region::CRb,{1.11E-08,-1.41E-04,1.05E+00}},
-            {Region::CRc,{4.96E-08,-3.38E-04,1.07E+00}}
-        };
-
-        std::map<Region,std::vector<double>> parametersMadGraphNLO = {
-            {Region::DefaultNoRW,{0.0,0.0,1.0}},
-            {Region::SR,{1.26E-07,-2.35E-04,1.19E+00}},
-            {Region::CRa,{1.69E-07,-2.58E-04,9.68E-01}},
-            {Region::CRb,{-3.62E-08,1.19E-04,7.73E-01}},
-            {Region::CRc,{-8.28E-08,1.61E-04,9.65E-01}}
-        };
-
         double mjj=sqrt(2*(ljet_0_p4->Dot(*ljet_1_p4)));
         double mjj_w=1;
-        double a{0.0};
-        double b{0.0};
-        double c{1.0};
-
+        
         // mjj reweighting
         MC mcSample = static_cast<MC>(z_sample);
         if(mcSample == MC::PowHegPythia){
             mjj_w = 1.0;
         } else if (mcSample == MC::SHERPA){
-            a = parametersSHERPA[region].at(0);
-            b = parametersSHERPA[region].at(1);
-            c = parametersSHERPA[region].at(2);
-            mjj_w = mjj_rw_quadratic(mjj,a,b,c);
+            mjj_w = mjj_rw(mjj,parametersSHERPA[region]); 
         } else if (mcSample == MC::MadGraph){ 
-            a = parametersMadGraph[region].at(0);
-            b = parametersMadGraph[region].at(1);
-            c = parametersMadGraph[region].at(2);
-            mjj_w = mjj_rw_quadratic(mjj,a,b,c);
+            mjj_w = mjj_rw(mjj,parametersMadGraph[region]);
         } else if (mcSample == MC::SHERPANLO){ 
-            a = parametersMadGraph[region].at(0);
-            b = parametersMadGraph[region].at(1);
-            c = parametersMadGraph[region].at(2);
-            mjj_w = mjj_rw_quadratic(mjj,a,b,c);
+            mjj_w = mjj_rw(mjj,parametersSHERPANLO[region]);
         } else if (mcSample == MC::MadGraphNLO){ 
-            a = parametersMadGraph[region].at(0);
-            b = parametersMadGraph[region].at(1);
-            c = parametersMadGraph[region].at(2);
-            mjj_w = mjj_rw_quadratic(mjj,a,b,c);
-        }
+            mjj_w = mjj_rw(mjj,parametersMadGraphNLO[region]);
+        } 
 				
         // ZpT reweighting
-
         double z_w=1;
         double zpt_weight=1/z_w;
 
