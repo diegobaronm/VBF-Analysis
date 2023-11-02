4acca9f76407d5add38233eaa8cf6e85e6b191c0
Diego Baron <diegoa_baronm@hotmail.com>
2023-11-02 17:36:17 +0000

    Reordering the output folders, not yet finished.

--- Scripts/compareShapes.cpp
+++ Scripts/compareShapes.cpp
@@ -1,30 +1,46 @@
+std::vector<double> g_binEdgesTrueMass = {0, 66, 76, 86, 96, 106, 116, 127, 138, 149, 160, 180, 200, 250, 300, 400, 500, 750, 1000};
+const char* g_titleTrueMass = "m_{true}(#tau,#tau) GeV";
 
-std::vector<double> g_binEdges = {0, 66, 76, 86, 96, 106, 116, 127, 138, 149, 160, 180, 200, 250, 300, 400, 500, 750, 1000};
+std::vector<double> g_binEdgesMassJJ = {0, 250, 500, 750, 1000, 1250, 1500, 2000, 3000, 4000, 5000};
+const char* g_titleMassJJ = "m_{jj} GeV";
+
+std::vector<double> g_binEdges = g_binEdgesMassJJ;
 
 TCanvas *g_canvas = new TCanvas("c1", "c1", 600, 600);
 bool g_rebin = true;
 bool g_plotDensity = false;
-int g_nbins = 65;
+bool g_simpleBinning = false;
+int g_nbins = g_binEdges.size()-1;
+float g_binNormalisation = 250.0;
 std::string g_units = " GeV";
-const char* g_title = "m_{true}(#tau,#tau) GeV";
+const char* g_title = g_titleMassJJ;
 
 void plot(TCanvas *canvas, const char* fileName, const char* histogramName, const char* title,EColor color, const char* drawingOptions){
     TFile *f1 = new TFile(fileName);
     TH1F* h1 = (TH1F*)f1->Get(histogramName);
+    
+    // Rebin the histogram
+    if (g_rebin) {
+        if (g_simpleBinning) h1 = (TH1F*)h1->Rebin(g_nbins);
+        else h1 = (TH1F*)h1->Rebin(g_nbins, "h1", g_binEdges.data());
+    }
 
-    if (g_rebin) h1 = (TH1F*)h1->Rebin(g_nbins);
     if (g_plotDensity) h1->Scale(1.0/h1->Integral(1,-1));
+
+    // Normalize the histogram if variable binning has been applied.
+
+    if (g_rebin && !g_simpleBinning) h1->Scale(g_binNormalisation, "width");
 
     h1->SetTitle(title);
 
     canvas->cd();
 
     h1->GetXaxis()->SetRange(1,h1->GetNbinsX()+1); //Draw overflow bin
-    h1->GetYaxis()->SetRangeUser(0,1.4);
+    h1->GetYaxis()->SetRangeUser(0.1,10000);
     h1->Draw(drawingOptions);
 
     h1->GetXaxis()->SetTitle(g_title);
-    std::string ytitle = std::string("Events/")+std::to_string(g_nbins)+g_units;
+    std::string ytitle = std::string("Events/")+std::to_string(g_binNormalisation)+g_units;
     h1->GetYaxis()->SetTitle(ytitle.c_str());
     h1->GetXaxis()->SetTitleSize(0.056);
     h1->GetYaxis()->SetTitleSize(0.056);
@@ -41,12 +57,19 @@ void plot(TCanvas *canvas, const char* fileName, const char* histogramName, cons
 
 void compareShapes(){
     // Comparing different BDTs
-    plot(g_canvas, "/Users/user/Documents/HEP/VBF-Analysis/TauTau/TauhadTaulep/High-Mass/TruthSignalNewBDT/Signal_PoPy.root", "true_mass_basic_all", "New BDT",kBlue, "SAME L HIST P");
-    plot(g_canvas, "/Users/user/Documents/HEP/VBF-Analysis/TauTau/TauhadTaulep/High-Mass/TruthSignalNewBDT/Signal_PoPy.root", "true_mass_basic_all", "New BDT",kBlue, "SAME L E1 X0");
-    plot(g_canvas, "/Users/user/Documents/HEP/VBF-Analysis/TauTau/TauhadTaulep/High-Mass/TruthSignalOriginalBDT/Signal_PoPy.root", "true_mass_basic_all", "Original BDT",kRed, "SAME L HIST P");
+    //plot(g_canvas, "/Users/user/Documents/HEP/VBF-Analysis/MuMu/Plots/Z-peak/SR/Zmumu_SherpaRW-Alternative.root", "mass_jj", "Alternative RW - EWjj PoPy + SF",kBlue, "SAME L HIST P");
+    //plot(g_canvas, "/Users/user/Documents/HEP/VBF-Analysis/MuMu/Plots/Z-peak/SR/Zmumu_SherpaRW-Alternative.root", "mass_jj", "Alternative RW - EWjj PoPy + SF",kBlue, "SAME L E1 X0");
+    plot(g_canvas, "/Users/user/Documents/HEP/VBF-Analysis/MuMu/Plots/Z-peak/SR/Zmumu_MGRW.root", "mass_jj", "Standard RW - EWjj Sherpa",kRed, "SAME HIST P E1");
+    plot(g_canvas, "/Users/user/Documents/HEP/VBF-Analysis/MuMu/Plots/Z-peak/SR/Zmumu_MGRW-Alternative.root", "mass_jj", "Alternative RW - EWjj PoPy + SF",kBlue, "SAME HIST P E1");
     //plot(g_canvas, std::string("SignalPow0.3"), kGreen, "SAME L E1 X0");
     //plot(g_canvas, std::string("SignalPow0.3"), kGreen, "SAME L HIST");
     
+
+
+    // Comparing different BDTs
+    //plot(g_canvas, "/Users/user/Documents/HEP/VBF-Analysis/MuMu/Plots/Z-peak/SR/Zmumu_SherpaRW-Alternative.root", "mass_jj", "Mass(j,j) shape",kBlue, "SAME L HIST P");
+    //plot(g_canvas, "/Users/user/Documents/HEP/VBF-Analysis/TauTau/TauhadTaulep/High-Mass/TruthSignalNewBDT/Signal_PoPy.root", "true_mass_basic_all", "New BDT",kBlue, "SAME L E1 X0");
+    //plot(g_canvas, "/Users/user/Documents/HEP/VBF-Analysis/TauTau/TauhadTaulep/High-Mass/TruthSignalOriginalBDT/Signal_PoPy.root", "true_mass_basic_all", "Original BDT",kRed, "SAME L HIST P");
 
     // Just one BDT
     //plotRatio(c1, std::string("SignalNOmT"),kBlue, "SAME E1 X0");
