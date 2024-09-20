0e1fba089492bae4529b0fd4979c0e1dfec9f01a
Diego Baron <diegoa_baronm@hotmail.com>
2024-09-20 16:43:40 +0100

    Getting rid off the prepare scripts for Tau channles. All will be now tracked from Analysis.C and CLoop.C.

--- EleTau/SR_prepare.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp MC/Analysis_SR.C MC/Analysis.C
-cp MC/backend/CLoop_SR.C MC/backend/CLoop.C
--- EleTau/prepare_CR.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp MC/Analysis_CR.C MC/Analysis.C
-cp MC/backend/CLoop_CR.C MC/backend/CLoop.C
