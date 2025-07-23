9c651a2c26afd8e74c8adc9c777bca6838ba5f13
Diego Baron <diegoa_baronm@hotmail.com>
2025-07-23 16:43:54 +0100

    Adding: 1. Channel ROOT file combination for Zll.

--- HandleIO/AddChannels.py
+++ HandleIO/AddChannels.py
@@ -45,12 +45,25 @@ def create_if_not_exists(directory):
                 ERROR.log("Error creating directory: ",directory)
                 exit(1)
 
-def add_potential_rw_samples(pairs_dict,list_of_samples, used_rw_samples):
+def add_potential_rw_samples(pairs_dict,list_of_samples, used_rw_samples, channel=None):
+
     for sample in list_of_samples:
+
+        # For Zll samples, we need to create two pairs: one for Zee and one for Zmumu
+        sample1 = None
+        sample2 = None
+        if channel is None: # Not for the tau channels
+            sample1 = sample
+            sample2 = sample
+        elif channel == "Zll":
+            sample1 = sample.replace("Zll","Zee")
+            sample2 = sample.replace("Zll","Zmumu")
+
+        # Create the pairs in the dictionary
         if used_rw_samples == True:
-            pairs_dict[sample+"RW"] = (sample+"RW", sample+"RW")
+            pairs_dict[sample+"RW"] = (sample1+"RW", sample2+"RW")
         else:
-            pairs_dict[sample] = (sample, sample)
+            pairs_dict[sample] = (sample1, sample2)
 
 
 
@@ -106,7 +119,7 @@ def main(menu_option, used_rw_samples):
         # Do the merging
         for result_sample, sample_pair in hadd_dictionary.items():
             INFO.log("Merging %s.root from %s.root and %s.root" % (result_sample, sample_pair[0], sample_pair[1]))
-            status = os.system("hadd -f %s.root %s.root %s.root" % (ztt_path+result_sample, zte_path+sample_pair[0], ztm_path+sample_pair[1]))
+            status = os.system("hadd -v1 -f %s.root %s.root %s.root" % (ztt_path+result_sample, zte_path+sample_pair[0], ztm_path+sample_pair[1]))
             if status:
                 ERROR.log("Error merging these samples!")
             else:
@@ -156,14 +169,70 @@ def main(menu_option, used_rw_samples):
         # Do the merging
         for result_sample, sample_pair in hadd_dictionary.items():
             INFO.log("Merging %s.root from %s.root and %s.root" % (result_sample, sample_pair[0], sample_pair[1]))
-            status = os.system("hadd -f %s.root %s.root %s.root" % (ztt_path+result_sample, zthtl_path+sample_pair[0], zemu_path+sample_pair[1]))
+            status = os.system("hadd -v1 -f %s.root %s.root %s.root" % (ztt_path+result_sample, zthtl_path+sample_pair[0], zemu_path+sample_pair[1]))
             if status:
                 ERROR.log("Error merging these samples!")
             else:
                 INFO.log("Sucess!!")
 
     elif menu_option==3: # Merge MuMu and Zee
-        # TODO
+        hadd_dictionary = {} # Zee in first position, MuMu in second position
+        # Fill with all the relevant pairings
+        hadd_dictionary["Data"] = ("Data","Data")
+        hadd_dictionary["Signal_Sherpa"] = ("Signal_Sherpa","Signal_Sherpa")
+        hadd_dictionary["Signal_PoPy"] = ("Signal_PoPy","Signal_PoPy")
+        hadd_dictionary["Signal_old"] = ("Signal_old","Signal_old")
+        hadd_dictionary["singletop"] = ("singletop","singletop")
+        hadd_dictionary["ttbar"] = ("ttbar","ttbar")
+        hadd_dictionary["VV"] = ("VV","VV")
+        hadd_dictionary["Wjets"] = ("Wjets","Wjets")
+        hadd_dictionary["Zjets"] = ("Zmumu","Zee")
+        hadd_dictionary["Ztautau"] = ("Ztautau_PoPy","Ztautau_PoPy")
+        hadd_dictionary["W_EWK_Sherpa"] = ("W_EWK_Sherpa","W_EWK_Sherpa")
+        hadd_dictionary["W_EWK_PoPy"] = ("W_EWK_PoPy","W_EWK_PoPy")
+        hadd_dictionary["VV_EWK"] = ("VV_EWK","VV_EWK")
+        potential_rw_samples = ["Zll_Sherpa","Zll_MG","Zll_SherpaNLO","Zll_MGNLO","Zll_PoPy"]
+        add_potential_rw_samples(hadd_dictionary, potential_rw_samples, used_rw_samples, channel="Zll")
+
+        # Ask the name of the directory where files are stored
+        dir_name = input("Enter the name of the directory where the files will be stored: ")
+        validate_input(dir_name)
+
+        # First the final directory
+        zll_path = os.path.join("Zll", dir_name)
+        create_if_not_exists(zll_path)
+
+        # Define the input paths
+        zee_path= os.path.join("Zee", dir_name)
+        create_if_not_exists(zee_path)
+        zmm_path= os.path.join("MuMu", dir_name)
+        create_if_not_exists(zmm_path)
+
+        # Copy the root files from the channels to the output directory
+        # Obtain the source paths relative to the current directory
+        INFO.log('Enter the path to the Zee samples (relative to the current directory):')
+        source_zee_path = input().strip()
+        if not os.path.exists(source_zee_path):
+            ERROR.log("The path %s does not exist. Please check the path and try again." % source_zee_path)
+            exit(1)
+        
+        INFO.log('Enter the path to the MuMu samples (relative to the current directory):')
+        source_mumu_path = input().strip()
+        if not os.path.exists(source_mumu_path):
+            ERROR.log("The path %s does not exist. Please check the path and try again." % source_mumu_path)
+            exit(1)
+
+        copy_root_files_from_to(source_zee_path, zee_path, list_of_potential_rw_samples = potential_rw_samples, used_rw_samples = used_rw_samples) # Relative to the current directory
+        copy_root_files_from_to(source_mumu_path, zmm_path, list_of_potential_rw_samples = potential_rw_samples, used_rw_samples = used_rw_samples)
+
+        # Do the merging
+        for result_sample, sample_pair in hadd_dictionary.items():
+            INFO.log("Merging %s.root from %s.root and %s.root" % (result_sample, sample_pair[0], sample_pair[1]))
+            status = os.system("hadd -v1 -f %s.root %s.root %s.root" % (os.path.join(zll_path, result_sample), os.path.join(zee_path, sample_pair[0]), os.path.join(zmm_path, sample_pair[1])))
+            if status:
+                ERROR.log("Error merging these samples!")
+            else:
+                INFO.log("Sucess!!")
 
     else:
         ERROR.log("Invalid option selected. Please choose a valid option.")
@@ -171,7 +240,7 @@ def main(menu_option, used_rw_samples):
 
 
 if __name__ == "__main__":
-    option = menu("Which channels do you want to merge?",["TauMu + EleTau","TauLep + MuEle (TauLep need to be already in the correct path)"," MuMu + Zee"])
+    option = menu("Which channels do you want to merge?",["TauMu + EleTau","TauLep + MuEle (TauLep need to be already in the correct path)","Zee + MuMu"])
     used_rw_samples = menu("Did you use RW samples?",["No","Yes"]) == 2
     main(option, used_rw_samples)
     
